b29c8b0ae5fb5afdd67f4ed6c3edb2a2f0e09195
ikebo <k_1043@126.com>
2021-02-19 17:21:19 +0800

    refresh nginx conf when start

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 nohup redis-server &
 source /board/venv/bin/activate
+cp nginx.conf /etc/nginx/nginx.conf
 nginx -c /etc/nginx/nginx.conf
 cd src && pip install -r requirements.txt && gunicorn -c gunicorn.conf.py app:app  >>runtime.log 2>&1
