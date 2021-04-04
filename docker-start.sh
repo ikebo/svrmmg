68fe8fcf873f3566695dffd7a8aadf28d2e937be
ikebo <k_1043@126.com>
2021-04-04 22:06:26 +0800

    update docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -1,1 +1,1 @@
-docker run -it -d --name svrmg -v $(pwd):/project -p 0.0.0.0:80:80/tcp -p 0.0.0.0:5000:5000/tcp egbertke/svrmg
+docker run -it -d --name svrmg -v $(pwd)/src/nginx.conf:/etc/nginx/nginx.conf -v $(pwd):/project -p 0.0.0.0:80:80/tcp -p 0.0.0.0:5000:5000/tcp egbertke/svrmg
