f2c97bfbb7ba66176815aaafc930363f62bb6cff
ikebo <k_1043@126.com>
2021-04-16 23:25:19 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@ nohup redis-server &
 source /board/venv/bin/activate
 cp nginx.conf /etc/nginx/nginx.conf
 nginx -c /etc/nginx/nginx.conf
-cd src && pip install -r requirements.txt && gunicorn -c gunicorn.conf.py app:app  >>runtime.log 2>&1
+cd src && pip install -r requirements.txt
+sh heart_beat_firer.sh
+gunicorn -c gunicorn.conf.py app:app  >>runtime.log 2>&1
