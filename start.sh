e33ddfec310ec14cf7fff1dffd9a076c1c107594
ikebo <k_1043@126.com>
2022-02-20 14:51:26 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 nohup redis-server &
 source /board/venv/bin/activate
 cp nginx.conf /etc/nginx/nginx.conf
+apt-get install -y ssh
+apt-get install -y expect
 nginx -c /etc/nginx/nginx.conf
 cd src && pip install -r requirements.txt
 gunicorn -c gunicorn.conf.py app:app  >>runtime.log 2>&1
