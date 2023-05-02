14bbd20f084cc9b09487ceff46d2ca6ab48be2e7
kebo <kebo@didiglobal.com>
2023-05-02 09:54:28 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,7 @@ cp nginx.conf /etc/nginx/nginx.conf
 #apt-get install -y expect
 nginx -c /etc/nginx/nginx.conf
 cd src && pip install -r requirements.txt
+supervisorctl stop beater
+supervisorctl stop recover
+ps -ef | grep bg.beats | grep -v color | awk '{print $2}' | xargs kill -9
 gunicorn -c gunicorn.conf.py app:app  >/dev/null 2>&1
